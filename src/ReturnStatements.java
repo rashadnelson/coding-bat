a9bcab4af4ba9ee9cac574a0ff01349863326cd7
Rashad Nelson <rashadnelson@gmail.com>
2022-05-19 09:46:41 -0400

    First commit

---
+++ src/ReturnStatements.java
@@ -0,0 +1,135 @@
+public class ReturnStatements {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
+    /*
+    CODING BAT - 5/19/22
+
+    METHOD CALLS
+    String strValue1 = nonStart("Hello", "There");
+    System.out.println(strValue1);
+
+    String strValue2 = left2("Hello");
+    System.out.println(strValue2);
+
+    String strValue3 = right2("Hello");
+    System.out.println(strValue3);
+
+    METHODS
+    public static String nonStart(String a, String b) {
+
+        int aLength = a.length();
+        int bLength = b.length();
+
+        String newA = a.substring(1, aLength);
+        String newB = b.substring(1, bLength);
+
+        return newA + newB;
+    }
+
+    public static String left2(String str) {
+
+        int strLength = str.length();
+
+        String firstTwo = str.substring(0, 2);
+
+        return str.substring(2, strLength) + firstTwo;
+    }
+
+    public static String right2(String str) {
+
+        int strLength = str.length();
+
+        int cut = str.length() - 2;
+
+        String firstChars = str.substring(0, cut);
+        String last2Chars = str.substring(cut);
+
+        return last2Chars + firstChars;
+
+    }
+
+     */
+
+
+
+
+    /*
+
+    // Code Belongs in Main Method
+        String shouting = caps("Why are you reading my diary mom?!");
+        System.out.println(shouting);
+
+        int[] awesomeArray = gimmeArrayfromInts(3, 7, 1);
+        System.out.println(awesomeArray[0]);
+        System.out.println(awesomeArray[1]);
+        System.out.println(awesomeArray[2]);
+
+        String ultimateCombo = comboString("Hello", "hi");
+        System.out.println(ultimateCombo);
+
+
+    // String Method Example
+    public static String caps(String s) {
+
+        return s.toUpperCase();
+    }
+
+    // Array Method Example
+    public static int[] gimmeArrayfromInts(int a, int b, int c ) {
+
+        int[] array = new int[3];
+        array[0] = a;
+        array[1] = b;
+        array[2] = c;
+
+        return array;
+    }
+
+    // String Method Example
+    public static String comboString(String a, String b) {
+
+        String temp = "";
+
+        if ( b.length() > a.length() ) {
+
+            // Store the shorter string
+            temp = a;
+
+            // "a" is now the larger string
+            a = b;
+
+            // "b" is now the shorter string
+            b = temp;
+
+            // Now we know that "a" is the larger string and "b" is the shorter string
+            return b + a + b;
+        }
+
+        if ( a.length() > b.length() ) {
+
+            // Store the shorter string
+            temp = b;
+
+            // "b" is now the larger string
+            b = a;
+
+            // "a" is now the shorter string
+            a = temp;
+
+            // Now we know that "b" is the larger string and "a" is the shorter string
+            return a + b + a;
+        }
+
+        return "";
+    }
+
+
+     */
+
+
